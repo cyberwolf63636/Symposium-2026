99b339e83b98964886c165a3a4625f6e3615c148
CybersecuritySKP <csecs149@gmail.com>
2026-02-04 12:26:06 +0530

    Deployment_changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // ✅ Switched back to BrowserRouter
 import App from './App.jsx'
 import './index.css'
 
