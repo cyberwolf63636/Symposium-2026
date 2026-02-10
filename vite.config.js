61750c20e6081786690445ff04cdd53939e766be
CybersecuritySKP <csecs149@gmail.com>
2026-02-10 12:23:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // No "base" line is needed for Vercel
+  build: {
+    chunkSizeWarningLimit: 1000, // ✅ Increases limit to 1000kb (1MB)
+  }
 })
